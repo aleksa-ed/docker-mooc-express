19b4c2611550c5d37f82eb3e1c053354ce078aa9
aleksa <aleksarh92@gmail.com>
2024-06-13 07:38:31 +0000

    change hello message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello University of Helsinki from Belgrade, Serbia!</h1>')
 })
 
 const PORT = 8080
